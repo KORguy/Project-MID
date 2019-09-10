50537a11f8c273503924bcab50a154d25765c179
KORguy <hyun.kang@hotmail.com>
2019-09-10 18:33:04 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
 
-export default class FlexDirectionBasics extends Component {
+export default class HelloWorldApp extends Component {
   render() {
     return (
-      // Try setting `flexDirection` to `column`.
-      <View style={{flex: 1, flexDirection: 'row'}}>
-        <View style={{width: 100, height: 500, backgroundColor: 'powderblue'}} />
-        <View style={{width: 100, height: 500, backgroundColor: 'skyblue'}} />
-        <View style={{width: 100, height: 500, backgroundColor: 'steelblue'}} />
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Hello, world!</Text>
       </View>
     );
   }
-};
+}
